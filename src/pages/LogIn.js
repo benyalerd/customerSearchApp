6841a63d0f9fbccd11ef4b2188b710386ee5f1bb
benya <benya.lerd@gmail.com>
2022-10-13 17:50:40 +0700

    wip : add login page

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -29,11 +29,38 @@ class LogIn extends React.Component{
   }
 
     render(){
-      return(          
-   <div>
-    <div>Log In</div>
-   </div>
+      return(     
+        
 
+ <div className={this.state.width <= 998 ?"":"div-center div-bg-singup"} style={this.state.width <= 998 ?{}:{height:this.state.height}}>
+
+ {/*Sign up Div*/}
+ 
+ <div className="div-singup"  style={this.state.width <= 998 ?{width:'100%' ,height:this.state.height, borderRadius:'0px'}:{}}>
+<div>
+     {/*Sign up Title*/}
+      <div className="text-title-pink" style={{marginBottom:'30px'}}>Log in</div>
+
+        {/*Email Input*/}
+      <input  type="email" class="form-control input"  id="InputEmail" placeholder="e-mail" />
+
+        {/*Password Input*/}
+      <input  type="password" class="form-control input"  id="InputPassword" placeholder="password" />
+      
+       {/*Log in Button*/}
+      <div className="div-center">
+      <button className="primary-button" style={{marginTop:'20px'}}>Log in</button>
+      </div>
+
+   {/*Register Link*/}
+  <div className="form-group"  style={{padding:'10px 0px',textAlign:'center'}}>
+    <a style={{cursor:'pointer',color:'#e6bd6f',textDecoration: 'underline'}}>Register</a>
+  </div>
+
+ </div>
+</div>
+
+</div>
       );
     }
   }
